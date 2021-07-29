4bdfaaff133f856e2b34c1d7a3185c8581539211
zhulinyi <zhulinyi@sics.ac.cn>
2021-07-29 11:41:44 +0800

    test_for_jenkins_hello_world

---
+++ src/main/java/org/example/JenkinsHelloWorld.java
@@ -0,0 +1,10 @@
+//package org.example;
+
+public class JenkinsHelloWorld {
+    public static void main(String[] args) {
+        for (int i = 0; i < 11; i++) {
+            System.out.println("hello world");
+        }
+
+    }
+}
